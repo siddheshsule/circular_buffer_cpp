052c181d2e2c0c22fb23d3c88f3e82d6056cacc6
siddhesh.sule47@gmail.com <siddhesh.sule47@gmail.com>
2024-01-18 21:38:27 +0100

    Initial commit

---
+++ include/circular_buffer.hpp
@@ -0,0 +1,14 @@
+#ifndef circular_buffer_hpp
+#define circular_buffer_hpp
+
+#include <iostream>
+#include <array>
+#include <stdexcept>
+
+template <typename T, int cap, typename Container = std::array<T, cap>>
+class CircularBuffer {
+    public:
+
+};
+
+#endif
